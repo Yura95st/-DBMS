612ac654b19f27f0664b9e65b664b9c94cfcc0d2
yura95st <yura95st@gmail.com>
2016-10-05 19:05:45 +0300

    Add Guard utility class.

---
+++ Domain/Utils/Guard.cs
@@ -0,0 +1,24 @@
+﻿namespace Domain.Utils
+{
+    using System;
+
+    /// <summary>
+    ///     A static helper class that includes various parameter checking routines.
+    /// </summary>
+    public static class Guard
+    {
+        /// <summary>
+        ///     Throws <see cref="ArgumentNullException" /> if the given argument is null.
+        /// </summary>
+        /// <exception cref="ArgumentNullException">The value is null.</exception>
+        /// <param name="argumentValue">The argument value to test.</param>
+        /// <param name="argumentName">The name of the argument to test.</param>
+        public static void NotNull(object argumentValue, string argumentName)
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName, String.Format("Argument '{0}' must not be null.", argumentName));
+            }
+        }
+    }
+}
