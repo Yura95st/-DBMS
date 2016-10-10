6d939bdf041cdc7083bc6afff4cc1e37746f9d7c
yura95st <yura95st@gmail.com>
2016-10-10 20:01:08 +0300

    Add implementation of IsValidDatabaseName and CheckAttribute methods of the IDatabaseValidation interface.

---
+++ App.Tests/Validations/Concrete/DatabaseValidationTests.cs
@@ -0,0 +1,146 @@
+﻿namespace App.Tests.Validations.Concrete
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using App.Exceptions;
+    using App.Models;
+    using App.Validations.Concrete;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DatabaseValidationTests
+    {
+        private DatabaseValidationSettings _dbValidationSettings;
+
+        [Test]
+        public void CheckAttribute_AttributeHasInvalidName_ThrowsInvalidAttributeException()
+        {
+            // Arrange
+            string[] attributeNames = { null, "", " ", new string(Path.GetInvalidFileNameChars()) };
+
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            foreach (string attributeName in attributeNames)
+            {
+                Models.Attribute attribute = new Models.Attribute { Name = attributeName };
+
+                Assert.Throws<InvalidAttributeException>(() => target.CheckAttribute(attribute));
+            }
+        }
+
+        [Test]
+        public void CheckAttribute_AttributeHasUnknownType_ThrowsInvalidAttributeException()
+        {
+            // Arrange
+            Models.Attribute attribute = new Models.Attribute { Name = "testAttribute", Type = "testType" };
+
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            Assert.Throws<InvalidAttributeException>(() => target.CheckAttribute(attribute));
+        }
+
+        [Test]
+        public void CheckAttribute_AttributeIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => target.CheckAttribute(null));
+        }
+
+        [Test]
+        public void CheckAttribute_AttributeIsValid_DoesNotThrowAnyException()
+        {
+            // Arrange
+            Models.Attribute attribute = new Models.Attribute
+                { Name = "testAttribute", Type = this._dbValidationSettings.DataTypes.Keys.First() };
+
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            Assert.DoesNotThrow(() => target.CheckAttribute(attribute));
+        }
+
+        [Test]
+        public void CheckAttribute_AttributeTypeIsNull_ThrowsInvalidAttributeException()
+        {
+            // Arrange
+            Models.Attribute attribute = new Models.Attribute { Name = "testAttribute", Type = null };
+
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+
+            Assert.Throws<InvalidAttributeException>(() => target.CheckAttribute(attribute));
+        }
+
+        [Test]
+        public void CheckTable_TableIsNull_ThrowsArgumentNullException()
+        {
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => target.CheckTable(null));
+        }
+
+        [Test]
+        public void CheckTable_TableHasInvalidName_ThrowsInvalidTableException()
+        {
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => target.CheckTable(null));
+        }
+
+        [SetUp]
+        public void Init()
+        {
+            this._dbValidationSettings =
+                new DatabaseValidationSettings(dataTypes:
+                    new Dictionary<string, Regex> { { "someType", new Regex(@"^-*[0-9]+$") } });
+        }
+
+        [Test]
+        public void IsValidDatabaseName_DatabaseNameIsInvalid_ReturnsFalse()
+        {
+            // Arrange
+            string[] dbNames = { null, "", " ", new string(Path.GetInvalidFileNameChars()) };
+
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            foreach (string dbName  in dbNames)
+            {
+                Assert.IsFalse(target.IsValidDatabaseName(dbName));
+            }
+        }
+
+        [Test]
+        public void IsValidDatabaseName_DatabaseNameIsValid_ReturnsTrue()
+        {
+            // Arrange
+            string dbName = "testDatabase";
+
+            // Arrange - create target
+            DatabaseValidation target = new DatabaseValidation(this._dbValidationSettings);
+
+            // Act and Assert
+            Assert.IsTrue(target.IsValidDatabaseName(dbName));
+        }
+    }
+}
---
+++ App/Validations/Concrete/DatabaseValidation.cs
@@ -0,0 +1,61 @@
+﻿namespace App.Validations.Concrete
+{
+    using System;
+    using System.IO;
+
+    using App.Exceptions;
+    using App.Models;
+    using App.Utils;
+    using App.Validations.Abstract;
+
+    public class DatabaseValidation : IDatabaseValidation
+    {
+        private readonly DatabaseValidationSettings _settings;
+
+        public DatabaseValidation(DatabaseValidationSettings settings)
+        {
+            Guard.NotNull(settings, "settings");
+
+            this._settings = settings;
+        }
+
+        #region IDatabaseValidation Members
+
+        public void CheckAttribute(Models.Attribute attribute)
+        {
+            Guard.NotNull(attribute, "attribute");
+
+            if (!DatabaseValidation.IsValidFileName(attribute.Name))
+            {
+                throw new InvalidAttributeException($"Attribute's name \"{attribute.Name}\" is invalid.");
+            }
+
+            if (attribute.Type == null || !this._settings.DataTypes.ContainsKey(attribute.Type))
+            {
+                throw new InvalidAttributeException($"Attribute's type \"{attribute.Type}\" is unknown.");
+            }
+        }
+
+        public void CheckRow(Table table, Row row)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckTable(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValidDatabaseName(string dbName)
+        {
+            return DatabaseValidation.IsValidFileName(dbName);
+        }
+
+        #endregion
+
+        private static bool IsValidFileName(string fileName)
+        {
+            return !string.IsNullOrWhiteSpace(fileName) && fileName.IndexOfAny(Path.GetInvalidFileNameChars()) < 0;
+        }
+    }
+}
