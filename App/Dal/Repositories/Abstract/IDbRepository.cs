77a9e622897e27cdf467febcb19fca57459464e0
yura95st <yura95st@gmail.com>
2016-10-13 22:41:15 +0300

    Add DbRepository.

---
+++ App/Dal/Repositories/Abstract/IDbRepository.cs
@@ -0,0 +1,19 @@
+﻿namespace App.Dal.Repositories.Abstract
+{
+    using System.Collections.Generic;
+
+    using App.Models;
+
+    public interface IDbRepository
+    {
+        void Create(Database database);
+
+        void Delete(string dbName);
+
+        Database GetByName(string dbName);
+
+        IEnumerable<string> GetAllNames();
+
+        void Update(Database database);
+    }
+}
---
+++ App/Exceptions/DbRepositoryException.cs
@@ -0,0 +1,25 @@
+﻿namespace App.Exceptions
+{
+    using System;
+
+    public class DbRepositoryException : Exception
+    {
+        #region Constructors
+
+        public DbRepositoryException()
+        {
+        }
+
+        public DbRepositoryException(string message)
+            : base(message)
+        {
+        }
+
+        public DbRepositoryException(string message, Exception inner)
+            : base(message, inner)
+        {
+        }
+
+        #endregion
+    }
+}
