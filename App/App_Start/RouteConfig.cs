b6d7e5fd3f8b61f246244494b7630922e4b236c5
yura95st <yura95st@gmail.com>
2016-10-13 17:40:38 +0300

    Add new routes to the RouteConfig.

--- App/App_Start/RouteConfig.cs
+++ App/App_Start/RouteConfig.cs
@@ -9,8 +9,24 @@
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(name: "Default", url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+            routes.MapRoute(name: null, url: "databases/{dbName}/{tableName}/editRow/{rowId}",
+                defaults: new { controller = "Home", action = "EditRow" });
+
+            routes.MapRoute(name: null, url: "databases/{dbName}/{tableName}/addRow",
+                defaults: new { controller = "Home", action = "AddRow" });
+
+            routes.MapRoute(name: null, url: "databases/{dbName}/{tableName}/scheme",
+                defaults: new { controller = "Home", action = "ShowTableScheme" });
+
+            routes.MapRoute(name: null, url: "databases/{dbName}/{tableName}",
+                defaults: new { controller = "Home", action = "ShowTable" });
+
+            routes.MapRoute(name: null, url: "databases/{dbName}",
+                defaults: new { controller = "Home", action = "ShowDatabase" });
+
+            routes.MapRoute(name: null, url: "databases", defaults: new { controller = "Home", action = "Index" });
+
+            routes.MapRoute(name: null, url: "{controller}/{action}", defaults: new { controller = "Home", action = "Index" });
         }
     }
 }
