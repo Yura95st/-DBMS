e9d9a84695daeae2705b37a8e4907cf1fd32d477
yura95st <yura95st@gmail.com>
2016-10-14 19:39:02 +0300

    Add Database and Table DTOs.

---
+++ App/DTOs/TableDto.cs
@@ -0,0 +1,34 @@
+﻿namespace App.DTOs
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using App.Models;
+
+    public class TableDto
+    {
+        public TableDto()
+        {
+            this.Attributes = new List<Attribute>();
+            this.Rows = Enumerable.Empty<Row>();
+        }
+
+        public IList<Attribute> Attributes
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public IEnumerable<Row> Rows
+        {
+            get;
+            set;
+        }
+    }
+}
