073cf6e9fce830217744bd376c36def9f4ce24e2
yura95st <yura95st@gmail.com>
2016-10-14 22:34:29 +0300

    Add Ninject packages.

---
+++ App/App_Start/NinjectWebCommon.cs
@@ -0,0 +1,73 @@
+using App;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(NinjectWebCommon), "Start")]
+[assembly: WebActivatorEx.ApplicationShutdownMethodAttribute(typeof(NinjectWebCommon), "Stop")]
+
+namespace App
+{
+    using System;
+    using System.Web;
+
+    using App.Validations.Abstract;
+    using App.Validations.Concrete;
+
+    using Microsoft.Web.Infrastructure.DynamicModuleHelper;
+
+    using Ninject;
+    using Ninject.Web.Common;
+
+    public static class NinjectWebCommon
+    {
+        private static readonly Bootstrapper bootstrapper = new Bootstrapper();
+
+        /// <summary>
+        ///     Starts the application
+        /// </summary>
+        public static void Start()
+        {
+            DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
+            DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
+            NinjectWebCommon.bootstrapper.Initialize(NinjectWebCommon.CreateKernel);
+        }
+
+        /// <summary>
+        ///     Stops the application.
+        /// </summary>
+        public static void Stop()
+        {
+            NinjectWebCommon.bootstrapper.ShutDown();
+        }
+
+        /// <summary>
+        ///     Creates the kernel that will manage your application.
+        /// </summary>
+        /// <returns>The created kernel.</returns>
+        private static IKernel CreateKernel()
+        {
+            StandardKernel kernel = new StandardKernel();
+            try
+            {
+                kernel.Bind<Func<IKernel>>()
+                    .ToMethod(ctx => () => new Bootstrapper().Kernel);
+                kernel.Bind<IHttpModule>()
+                    .To<HttpApplicationInitializationHttpModule>();
+
+                NinjectWebCommon.RegisterServices(kernel);
+                return kernel;
+            }
+            catch
+            {
+                kernel.Dispose();
+                throw;
+            }
+        }
+
+        /// <summary>
+        ///     Load your modules or register your services here!
+        /// </summary>
+        /// <param name="kernel">The kernel.</param>
+        private static void RegisterServices(IKernel kernel)
+        {
+        }
+    }
+}
