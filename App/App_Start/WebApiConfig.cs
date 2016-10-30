efb06fd3b80300f40d2700d51ae5d11534d46545
yura95st <yura95st@gmail.com>
2016-10-30 23:40:08 +0200

    Enable Cors in WebApi config.

--- App/App_Start/WebApiConfig.cs
+++ App/App_Start/WebApiConfig.cs
@@ -1,11 +1,15 @@
 ﻿namespace App
 {
     using System.Web.Http;
+    using System.Web.Http.Cors;
 
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:3000", "*", "*");
+            config.EnableCors(cors);
+
             // Web API configuration and services
 
             // Web API routes
